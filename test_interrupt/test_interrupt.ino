cd5119d9a0e38f77d3a3e391079dd32d54e68829
Bharat Tak <bharat.tak8@gmail.com>
2014-11-24 15:02:43 +0100

    Test commit. Tick logic removed, all falling edges recorded. Both interrupts working simultaneously, recoring gibbrish (obviously) but running smoothly, also verified with i2c.

--- test_interrupt/test_interrupt.ino
+++ test_interrupt/test_interrupt.ino
@@ -1,9 +1,10 @@
 #include "test_interrupt.h"
 #include <Wire.h>
 
-int counter_overflow[2];
-bool is_in_pwm[2];
-bool is_waiting_for_tick[2];
+//made volatile
+volatile int counter_overflow[2];
+volatile bool is_in_pwm[2];
+volatile bool is_waiting_for_tick[2];
 
 uint8_t RISING_EDGE1 = (1 << ISC11) | (1 << ISC10); //0b00001100
 uint8_t FALLING_EDGE1 = (!RISING_EDGE1) | (1 << ISC11); // 0b11111011 only works if before was rising edge
@@ -119,16 +120,24 @@ void requestEvent()
 
 ISR(INT1_vect)
 {
+  digitalWrite(13,HIGH);
+
+    unsigned int time = TCNT2;  
     unsigned int ovf = counter_overflow[1];
-    unsigned int time = TCNT2;
+    
     TCNT2 = 0;    //reset time
     counter_overflow[1] = 0;
     time = time + (ovf << 8);
 
+  tick(time,1);//testing only should be 1
+
+  /*
+
     if (is_waiting_for_tick[1]) {
         tick(time,1);
+        digitalWrite(13,HIGH);
         //        TIMSK2 &= DISABLE_OVF;
-        TCCR2B = 0; // prescale remover or timer clock stopped ? still looking for falling edge.
+        TCCR2B = 0; // prescale remover or timer clock stopped ? does it stop the clock or just remove prescale.
         is_waiting_for_tick[1] = false;
         return;
     }
@@ -142,15 +151,19 @@ ISR(INT1_vect)
         EICRA &= FALLING_EDGE1; // look for falling edge
         return;
     }
+    
+    */
 }
 
 
 ISR(TIMER2_OVF_vect)
 {
-  //  my_ctr = my_ctr + 1;
-    digitalWrite(13,HIGH);
-
     counter_overflow[1]++;
+
+
+  //  my_ctr = my_ctr + 1;
+    /*
+
     //digitalWrite(13,HIGH);    
     
     if (counter_overflow[1] == 3 && digitalRead(3) == LOW) { //time taken = (2power8 = 256) x (prescale=8) / (clockfreq = 8Mhz) = 256 microsec per overflow. 3 overflow = 3*256 microsec = 0.7 ms low bottom we are in pwm 
@@ -173,20 +186,31 @@ ISR(TIMER2_OVF_vect)
         counter_overflow[1] = 0;
         return;
     }
+    
+    */
 }
 
 
 ISR(INT0_vect) 
 {
+  digitalWrite(13,LOW);
+  unsigned int time = TCNT1;
   unsigned int ovf = counter_overflow[0];
-    unsigned int time = TCNT1;
+
     TCNT1 = 0;    //reset time
     counter_overflow[0] = 0;
     time = time + (ovf << 8);
 
+
+    tick(time,0);
+  
+  /*
+
     if (is_waiting_for_tick[0]) {
         //digitalWrite(13, HIGH);
         tick(time,0);
+        digitalWrite(13,LOW);
+        
         //        TIMSK2 &= DISABLE_OVF;
         TCCR1B = 0;
         is_waiting_for_tick[0] = false;
@@ -202,17 +226,22 @@ ISR(INT0_vect)
         EICRA &= FALLING_EDGE0;
         return;
     }
+    
+    */
 }
 
 
-ISR(TIMER1_COMPA_vect) // Should not happen when connected to ESC
+ISR(TIMER1_COMPA_vect)
 {
+    counter_overflow[0]++;
+
+  
+  /*
   // my_ctr = my_ctr + 1;
-   digitalWrite(13,LOW);
+    
     //unsigned int time = TCNT1;
     //TCNT1 = 0;
     //Serial.println(time);
-    counter_overflow[0]++;
     //digitalWrite(13, HIGH);
     //TCNT1 = 0;    //reset time. Time resets after over flow.
 
@@ -237,6 +266,8 @@ ISR(TIMER1_COMPA_vect) // Should not happen when connected to ESC
         counter_overflow[0] = 0;
         return;
     }
+    
+    */
 }
 
 
@@ -263,6 +294,7 @@ inline void tick(unsigned int ticks, int index)
 
     if (d->frameIdx == DATA_FRAME_CNT - 1) {
         flag_ready[index] = true;
+        d->frameIdx = FRAME_RESET;//TEST THIS
         //  Serial.println("READY");
     }
 }
@@ -350,7 +382,8 @@ uint8_t getData(uint8_t index, CASTLE_ESC_DATA *o)
         // Why wait for reset() to be called from timer-overflow ? Without this, random ticks are 
         // recorded(with no pwm connected!) and after the required nuumber of them, flag is set. 
         // Some check is required that says the recorded batch of ticks should be within so much time.
-        flag_ready[index] = false; 
+        flag_ready[index] = false;
+        
         return true;
 
     } else return false;

0c96e3b16101231fd90c013fe4c44760b8ac5941
Bharat Tak <bharat.tak8@gmail.com>
2014-11-25 13:21:16 +0100

    Working clean code. LED blinks when both ESCs are connected and ticking. Serial output of RPM of both ESC.

--- test_interrupt/test_interrupt.ino
+++ test_interrupt/test_interrupt.ino
@@ -6,11 +6,11 @@ volatile int counter_overflow[2];
 volatile bool is_in_pwm[2];
 volatile bool is_waiting_for_tick[2];
 
-uint8_t RISING_EDGE1 = (1 << ISC11) | (1 << ISC10); //0b00001100 //dec 12
-uint8_t FALLING_EDGE1 = (!RISING_EDGE1) | (1 << ISC11); // 0b11111011 //dec 251
+uint8_t RISING_EDGE1 = 0b00001100;//(1 << ISC11) | (1 << ISC10); //0b00001100 //dec 12 //EICRA configuration check datasheet
+uint8_t FALLING_EDGE1 = 0b11111011;//(!RISING_EDGE1) | (1 << ISC11); // 0b11111011 //dec 251  //EICRA configuration check datasheet
 
-uint8_t RISING_EDGE0 = (1 << ISC01) | (1 << ISC00); //0b00000011 // dec 3
-uint8_t FALLING_EDGE0 = (!RISING_EDGE0) | (1 << ISC01); // 0b11111110 // dec 254
+uint8_t RISING_EDGE0 = 0b00000011;//(1 << ISC01) | (1 << ISC00); //0b00000011 // dec 3 //EICRA configuration check datasheet
+uint8_t FALLING_EDGE0 = 0b11111110;//(!RISING_EDGE0) | (1 << ISC01); // 0b11111110 // dec 254 //EICRA configuration check datasheet
 
 typedef struct castle_priv_data_struct {
     unsigned int ticks[DATA_FRAME_CNT];
@@ -48,41 +48,44 @@ void setup()
         flag_ready[i] = false;
     }
 
-    pinMode(13, OUTPUT);//test led blink
+    pinMode(13, OUTPUT);
     digitalWrite(13, LOW);
   
     pinMode(3, INPUT_PULLUP);
     pinMode(2, INPUT_PULLUP);
     Serial.begin(2400);
     cli();
-    TIMSK2 |= (1<<TOIE2); //Enable overflow
     
-    //OCR1A = 256;
-    ICR1 = 256;
-    TIFR1 |= _BV(OCF1A);      // clear any pending interrupts; 
-    TIMSK1 |=  _BV(OCIE1A) ;  // enable the output compare interrupt 
-    EICRA |=  (1 << ISC11) | (1 << ISC01); //Falling Edge for both interrupt INT0 and interrupt INT1 //0b00001010 //dec 
-    EIMSK |= (1 << INT1); //| (1 << INT0); //Enable interrupt via pin for INT0 and INT1
-
-    //EICRA |=  (1 << ISC11);
-    EIMSK |= (1 << INT0);
-
+    //Configure Timer2 (8bit). Over flow at 0-255. 
+    TIMSK2 |= (1<<TOIE2);                             // Enable overflow
     TCCR2A = 0;
     TCCR2B = 0;
+    TCCR2B |= 0b010;                                  // 8 prescaler 0b010;
+
+    //Configure Timer1 (16bit). CTC reset at TOP 256.
+    ICR1 = 256;                                       // Comparator TOP value. Timer1 (16bit) should reset in CTC mode via TCCR1A and TCCR1B values. Check datasheet.
+    TIFR1 |= _BV(OCF1A);                              // Clear any pending interrupts; 
+    TIMSK1 |=  _BV(OCIE1A) ;                          // Enable the output compare interrupt 
     TCCR1A = 0;
     TCCR1B = 0;
-    TCCR2B |= 0b010;    // 8 prescaler 0b010;
     TCCR1A |= 0b00;
-    TCCR1B |= 0b11010;     // set prescaler of 8. Time for overflow = 65ms. Should never happen when connected to esc. //higher prescale slower clock, more time for overflow, more difficult to characterize pwm or tick etc based on overflow as you have done (magic numbers 3 and 28 for 8bit timer with prescale 8). Needs be adjusted for 16bit timer with no prescale.
+    TCCR1B |= 0b11010;                                // Set prescaler of 8.
+
+    //Configure Interrupts at falling edge for both interrupts.
+    EICRA |=  (1 << ISC11) | (1 << ISC01);            // Falling Edge for both interrupt INT0 and interrupt INT1 //0b00001010 //dec 
+
+    EIMSK |= (1 << INT1);                             // Enable interrupt via pin for INT1 configured to time with Timer2
+    EIMSK |= (1 << INT0);                             // Enable external interrupt on INT0, times with Timer1.
+    
+    
     Wire.begin(2);
-    Wire.onRequest(requestEvent); // register event
+    Wire.onRequest(requestEvent);                     // Register event
     Serial.println("Start");
     sei();
 }
 
 void loop()
 {
-    //  delay(400);
     
     if(getData(0, &escdata[0])){
       Serial.println(escdata[0].RPM);
@@ -96,29 +99,37 @@ void loop()
       i2cdata = (byte *)data_float;  
     }
     
-    //Serial.println(my_ctr);
     delay(20);
-//    getData(0, &escdata[1]);
-    
-/*
-    Serial.println("H");
-    ctr_data[loop_ctr]= my_ctr;
-    loop_ctr++;
-    if(loop_ctr == 3){
-        loop_ctr = 0;
-        Serial.println(ctr_data[0]);
-        Serial.println(ctr_data[1]);
-        Serial.println(ctr_data[2]);
-        
-    }
-    */
-        
 }
 
 void requestEvent()
 {
-    Wire.write(i2cdata, 48); // respond with message of 6 bytes
+    Wire.write(i2cdata, 48);                          // Respond with message of 6 bytes
 }
+ 
+
+/* A     B  C  D E     F       G  H   I J     K      L  M   N       O       P  Q   R  S     T
+ * ______       _ _____________        _ ____________        _______________        __ _________
+ *       |_____| |             |______| |            |______|               |______|  |   
+ *
+ * The above waveform is what is expected out of the ESC. BCD, GHI, LMN & PQR represent the PWM 
+ * values. The RPM/Voltage/Current etc information is encoded in the position of a batch of 'Ticks' 
+ * positioned after the PWMs, represented here by E,J and S. We basically want to measure the time
+ * interval between the end of PWM and start of 'Tick', here length of DE, IJ and RS.
+ *
+ * The logic for detecting the time to 'Ticks', or just calling them 'Ticks', is exactly the same
+ * for both Timer2 (8 bit) and Timer1 (16 bit). The wave-forms (one for each ESC, on PIN2 and PIN3) 
+ * is sampled every 256 colck cycles prescaled by value 8 prescaler. That, for 8MHz clock is around 
+ * 256 micro seconds. This sampling is done by over-flow interrup for Timer2 and COMPA interrupt for
+ * Timer1. Over-flow counters are incremented to keep track of the length of time.
+ *
+ * The logic below can be verified by manually running through it with the waveform given above. Key
+ * values to note in the waveform are:
+ * Waveform cycle (eg DI or IN or NR) = 20 milli seconds
+ * Minimum length of PWM (eg BD or GI or LN or PR) = 0.7 milli seconds (over-flow > 3)
+ * Maximum time to 'Tick', when Tick is present (not RESET as in NP) < 7 milli seconds  (over-flow < 28)
+ */
+
 
 
 ISR(INT1_vect)
@@ -127,31 +138,27 @@ ISR(INT1_vect)
     unsigned int time = TCNT2;  
     unsigned int ovf = counter_overflow[1];
     
-    TCNT2 = 0;    //reset time
+    TCNT2 = 0;                                        // Reset time
     counter_overflow[1] = 0;
     time = time + (ovf << 8);
 
-  //tick(time,1);//testing only should be 1
+    //my_ctr = my_ctr + 1;                            // For debug only
 
-  my_ctr = my_ctr + 1;
-
-    if (is_waiting_for_tick[1]) {
+    if (is_waiting_for_tick[1]) {                     // Enter at E J S 
         tick(time,1);
         digitalWrite(13,LOW);
-        //        TIMSK2 &= DISABLE_OVF;
-        TCCR2B = 0; // prescale remover or timer clock stopped ? does it stop the clock or just remove prescale.
+        TCCR2B = 0; 
         is_waiting_for_tick[1] = false;
         return;
     }
-    //    TIMSK2 |= ENABLE_OVF;
     
-    TCCR2B = 0b10;//set prescale 8 (timer clock start or already running ? )
+    TCCR2B = 0b10;
 
-    if (is_in_pwm[1]) {
+    if (is_in_pwm[1]) {                              // Enter at B G L P
         
         is_waiting_for_tick[1] = true;
         is_in_pwm[1] = false;
-        EICRA &= 0b11111011;//FALLING_EDGE1; // look for falling edge  
+        EICRA &= FALLING_EDGE1;   
         return;
     }
     
@@ -163,28 +170,21 @@ ISR(TIMER2_OVF_vect)
 {
     counter_overflow[1]++;
 
-
-  //  my_ctr = my_ctr + 1;
-    
-
-    //digitalWrite(13,HIGH);    
-    
-    if (counter_overflow[1] == 3 && digitalRead(3) == LOW) { //time taken = (2power8 = 256) x (prescale=8) / (clockfreq = 8Mhz) = 256 microsec per overflow. 3 overflow = 3*256 microsec = 0.7 ms low bottom we are in pwm 
-        //            TIMSK2 &= DISABLE_OVF;
-        TCCR2B = 0; //why ? stopping clock ?
-        EICRA |= 0b00001100;// RISING_EDGE1;//  
+    // Time taken = (2power8 = 256) x (prescale=8) / (clockfreq = 8Mhz) = 256 microsec per overflow. 
+    // 3 overflow = 3*256 microsec = 0.7 ms low bottom we are in pwm 
+    if (counter_overflow[1] == 3 && digitalRead(3) == LOW) {  // Enter at C H M Q
+        TCCR2B = 0; 
+        EICRA |= RISING_EDGE1;
         is_in_pwm[1] = true;
         counter_overflow[1] = 0;
         return;
     }
-
-    if (counter_overflow[1] >= 28) { // no falling edge for over 7ms after rising edge
+    // No falling edge for over 7ms after rising edge
+    if (counter_overflow[1] >= 28) {                         // Enter at O
         if (digitalRead(3) == HIGH && is_waiting_for_tick[1]) {
-            //                   TIMSK2 &= DISABLE_OVF;
-            TCCR2B = 0; //stop timer clock
+            TCCR2B = 0; 
             reset(1);
             is_waiting_for_tick[1] = false;
-            //digitalWrite(13,HIGH); 
         }
 
         counter_overflow[1] = 0;
@@ -196,9 +196,7 @@ ISR(TIMER2_OVF_vect)
 
 
 ISR(INT0_vect) 
-{
-  //digitalWrite(13,LOW);
-  
+{  
   unsigned int time = TCNT1;
   unsigned int ovf = counter_overflow[0];
 
@@ -207,23 +205,20 @@ ISR(INT0_vect)
     time = time + (ovf << 8); 
   
 
-    if (is_waiting_for_tick[0]) {
+    if (is_waiting_for_tick[0]) {                          // Enter at E J S
         digitalWrite(13, HIGH);
         tick(time,0);
-        
-        //        TIMSK2 &= DISABLE_OVF;
         TCCR1B = 0;
         is_waiting_for_tick[0] = false;
         return;
     }
 
-    //    TIMSK2 |= ENABLE_OVF;
-    TCCR1B = 0b11010; 
+    TCCR1B = 0b11010;                                      // Start prescaler, keep CTC mode as it is
 
-    if (is_in_pwm[0]) {
+    if (is_in_pwm[0]) {                                    // Enter at B G L P
         is_waiting_for_tick[0] = true;
         is_in_pwm[0] = false;
-        EICRA &= 0b11111110 ;//FALLING_EDGE0;
+        EICRA &= FALLING_EDGE0;
         return;
     }
     
@@ -235,34 +230,22 @@ ISR(TIMER1_COMPA_vect)
 {
     counter_overflow[0]++;
 
-  
-  
-  // my_ctr = my_ctr + 1;
-    
-    //unsigned int time = TCNT1;
-    //TCNT1 = 0;
-    //Serial.println(time);
-    //digitalWrite(13, HIGH);
-    //TCNT1 = 0;    //reset time. Time resets after over flow.
-
-
-    if (counter_overflow[0] == 3 && digitalRead(2) == LOW) { //counter overflow 2 or 3 ??
-        //            TIMSK2 &= DISABLE_OVF;
+    // Time taken = (2power8 = 256) x (prescale=8) / (clockfreq = 8Mhz) = 256 microsec per overflow. 
+    // 3 overflow = 3*256 microsec = 0.7 ms low bottom we are in pwm
+    if (counter_overflow[0] == 3 && digitalRead(2) == LOW) {  // Enter at C H M Q
         TCCR1B = 0;
-        EICRA |= 0b00000011;//RISING_EDGE0;
+        EICRA |= RISING_EDGE0;
         is_in_pwm[0] = true;
         counter_overflow[0] = 0;
         return;
     }
 
-    if (counter_overflow[0] >= 28) {
+    if (counter_overflow[0] >= 28) {                          // Enter at O
         if (digitalRead(2) == HIGH && is_waiting_for_tick[0]) {
-            //                   TIMSK2 &= DISABLE_OVF;
             TCCR1B = 0;
             reset(0);
             is_waiting_for_tick[0] = false;
         }
-
         counter_overflow[0] = 0;
         return;
     }
@@ -276,21 +259,17 @@ void reset(int index)
 {
     CASTLE_PRIV_DATA *d = &(data[index]);
     d->frameIdx = FRAME_RESET;
-    //  Serial.println("reset");
-    //if castle ESC ticked some data in, reset the ticked indicator for next cycle
     flag_ready[index] = false;
 }
 
 
 inline void tick(unsigned int ticks, int index)
 {
-    //Serial.println(ticks);
     if (ticks == 0) { return; } //timer was stopped
 
     CASTLE_PRIV_DATA *d = &(data[index]);
     d->frameIdx++;
     d->ticks[d->frameIdx] = ticks;
-    //  Serial.println(d->ticks[0]);
 
     if (d->frameIdx == DATA_FRAME_CNT - 1) {
         flag_ready[index] = true;
@@ -298,22 +277,6 @@ inline void tick(unsigned int ticks, int index)
         //  Serial.println("READY");
     }
 }
-
-/*
-
-uint8_t getData(uint8_t index, CASTLE_RAW_DATA *o)
-{
-    if (flag_ready[index]) { //wait for ISRs code to finish filling data structure. Stops the main loop, nothing else is procesed if not ready. Why not revisit when ready ?
-      EIMSK = 0;
-      memcpy(o, data[index].ticks, sizeof(unsigned int) * DATA_FRAME_CNT);
-      EIMSK  |= (1 << INT1) ;//| (1 << INT1);
-      return true;
-    }else {
-      return false;
-    }
-
-}*/
-
 
 uint8_t getData(uint8_t index, CASTLE_ESC_DATA *o)
 {
@@ -378,10 +341,7 @@ uint8_t getData(uint8_t index, CASTLE_ESC_DATA *o)
             }
         }
         
-        // Reset flag immediately. Only true again when next complete batch of ticks are recorded. 
-        // Why wait for reset() to be called from timer-overflow ? Without this, random ticks are 
-        // recorded(with no pwm connected!) and after the required nuumber of them, flag is set. 
-        // Some check is required that says the recorded batch of ticks should be within so much time.
+        // Reset flag immediately.
         flag_ready[index] = false;
         
         return true;
